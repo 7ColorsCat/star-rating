10d37fb0207e64836aaf60366133c457078ed693
katinbox <tranquanki28@gmail.com>
2023-03-22 16:56:30 +0700

    style: thank you page

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,10 +1,18 @@
-import { Box, Image } from '@chakra-ui/react'
-import thankyouBg from '../assets/thankyou.jpg'
+import { Heading, Center, Link, VStack } from '@chakra-ui/react'
+import { Link as Rlink } from 'react-router-dom'
 
 export default function Thankyou() {
     return (
-        <Box h={'100vh'}>
-            <Image src={thankyouBg} objectFit={'cover'} />
-        </Box>
+        <Center
+            h={'100vh'}
+            bgGradient="linear-gradient(to right, #1d976c, #93f9b9)"
+        >
+            <VStack>
+                <Heading color={'white'}>Cảm ơn quý khách !</Heading>
+                <Link as={Rlink} to={'/'}>
+                    Đánh giá mới
+                </Link>
+            </VStack>
+        </Center>
     )
 }
